dea3c9c448ac2f8e6e037d5bc3768147eb92f107
Anastasis Germanidis <agermanidis@gmail.com>
2019-01-30 16:15:35 -0500

    updates for new runway sdk

--- model.py
+++ model.py
@@ -4,15 +4,20 @@ import torch
 import architecture as arch
 from runway import RunwayModel
 
+
 if torch.cuda.is_available():
+    print('Using GPU')
     device = torch.device('cuda')
 else:
+    print('Using CPU')
     device = torch.device('cpu')
+
 
 esrgan = RunwayModel()
 
-@esrgan.setup
-def setup(alpha=0.5):
+
+@esrgan.setup(options={'interpolationFactor': 'float'})
+def setup(opts):
     net_PSNR_path = './models/RRDB_PSNR_x4.pth'
     net_ESRGAN_path = './models/RRDB_ESRGAN_x4.pth'
     net_PSNR = torch.load(net_PSNR_path)
@@ -20,7 +25,7 @@ def setup(alpha=0.5):
     net_interp = OrderedDict()
     for k, v_PSNR in net_PSNR.items():
         v_ESRGAN = net_ESRGAN[k]
-        net_interp[k] = (1 - alpha) * v_PSNR + alpha * v_ESRGAN
+        net_interp[k] = (1 - opts['interpolationFactor']) * v_PSNR + opts['interpolationFactor'] * v_ESRGAN
     model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=4, norm_type=None, act_type='leakyrelu', \
                         mode='CNA', res_scale=1, upsample_mode='upconv')
     model.load_state_dict(net_interp)
@@ -28,6 +33,7 @@ def setup(alpha=0.5):
         v.requires_grad = False
     model = model.to(device)
     return model
+
 
 @esrgan.command('upscale', inputs={'image': 'image'}, outputs={'upscaled': 'image'})
 def upscale(model, inp):
@@ -40,6 +46,7 @@ def upscale(model, inp):
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round().astype('uint8')
     return dict(upscaled=output)
+
 
 if __name__ == '__main__':
     esrgan.run()
